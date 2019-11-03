a8cca7266dadcd2f9ca652f651305fd591a5ab08
Iyad Kandalaft <ik@iyadk.com>
2019-11-03 10:09:19 -0500

    Added search limit to permit setting number of returned results and improved buffer output performance

--- src/main/java/com/iyadk/termsearch/SearchCommand.java
+++ src/main/java/com/iyadk/termsearch/SearchCommand.java
@@ -7,18 +7,28 @@ import java.util.concurrent.Callable;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 
+/*
+ * search subcommand that is used to trigger searches of a lucene index based on a set of terms 
+ */
 @Command(name = "search")
 public class SearchCommand implements Callable<Integer> {
 	@Option(names={"-i", "--index", "--index-path"}, 
 			description="Path to read the index from", 
-			defaultValue = "./lucene-index")
+			defaultValue = "./lucene-index",
+			paramLabel="INDEXPATH")
 	private String indexDir;
 	
 	@Option(names={"-t", "--terms", "--search-terms-path"}, 
-			description="Path to the file of search times", 
-			defaultValue = "./terms.txt")
+			description="Path to the terms file to query for", 
+			defaultValue = "./terms.txt",
+			paramLabel="FILE")
 	private String termsFile;
-	
+
+	@Option(names={"-o", "--output", "--output-file"},
+			description="Path to the file where the output will be written",
+			defaultValue="./output/results.tsv",
+			paramLabel="FILE")
+	private String outputFile;
 	
 	public Integer call() throws Exception {
 		System.out.println("Executing search command");
@@ -26,14 +36,15 @@ public class SearchCommand implements Callable<Integer> {
 		SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
 		
 		System.out.println("Start time: " + dateFormatter.format(new Date()));
-		System.out.println("-".repeat(80));
+		System.out.println("-------------------------------------------------------");
 		
 		
-		SearchIndex indexSearcher = new SearchIndex(termsFile);
-		indexSearcher.searchAll();
+		SearchIndex indexSearcher = new SearchIndex(termsFile, outputFile);
+		indexSearcher.searchAll("content");
+		
+		indexSearcher.close();
 		
 		System.out.println("End time: " + dateFormatter.format(new Date()));
-		
 		
 		return 0;
 	}
--- src/main/java/com/iyadk/termsearch/SearchIndex.java
+++ src/main/java/com/iyadk/termsearch/SearchIndex.java
@@ -1,76 +1,151 @@
 package com.iyadk.termsearch;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.queryparser.classic.ParseException;
-import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
+import org.apache.lucene.search.PhraseQuery;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.MMapDirectory;
 
 public class SearchIndex {
 	private Path termsPath;
 	private Path indexPath;
+	private Path outputPath;
+	private int searchLimit = 100;
+
 	private Directory dirIndex;
+	private IndexReader reader;
+	public IndexSearcher searcher;
 	
+	/*
+	 * @param terms Path to file with each search phrase/term per line
+	 */
 	public SearchIndex(String terms) throws IOException {
-		this(terms, "./lucene-index");
+		this(terms,"output.tsv");
+	}
+
+	/*
+	 * @param terms Path to file with each search phrase/term per line
+	 * @param output Path to output file where results are written
+	 */
+	public SearchIndex(String terms, String output) throws IOException {
+		this(terms, output, "./lucene-index");
 	}
 	
-	public SearchIndex(String terms, String index) throws IOException {
+	/*
+	 * @param terms Path to file with each phrase/term per line
+	 * @param output Path to output file where results are written
+	 * @param index Path to lucene index directory
+	 */
+	public SearchIndex(String terms, String output, String index) throws IOException {
 		termsPath = Paths.get(terms);
+		outputPath = Paths.get(output);
 		indexPath = Paths.get(index);
+
+		/* Instantiate searcher using memory mapped directory*/
 		dirIndex = MMapDirectory.open(indexPath);
+		reader = DirectoryReader.open(dirIndex);
+		searcher = new IndexSearcher(reader);
 	}
 	
-	public void searchAll() throws IOException, ParseException {
-		IndexReader reader = DirectoryReader.open(FSDirectory.open(indexPath));
-		IndexSearcher searcher = new IndexSearcher(reader);
-		Analyzer analyzer = new StandardAnalyzer();
+	public int getSearchLimit() {
+		return searchLimit;
+	}
+
+	public void setSearchLimit(int searchLimit) {
+		this.searchLimit = searchLimit;
+	}
+
+	/*
+	 * Search for all the terms/phrases in the supplied terms file
+	 * @param field Name of the field to search
+	 */
+	public void searchAll(String field) throws IOException {
+		File outputFile = outputPath.toFile();
+
+		// Create the parent directory directory of the output file
+		if (! outputFile.getParentFile().exists())
+			outputFile.getParentFile().mkdirs();
+
+		// Create the output file
+		if (! outputFile.exists())
+			outputFile.createNewFile();
+
+		/*
+		 * Write output to a file using buffering.  Large memory buffers reduce IOPS and blocking.
+		 */
+		FileWriter fileWriter = new FileWriter(outputFile);
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter, 2^24);
+
+		// Use the correct line seperator based on the operating system
+		String lnSeperator = System.lineSeparator();
 		
 	    try ( BufferedReader in = Files.newBufferedReader(termsPath, StandardCharsets.UTF_8) ){
-		    QueryParser parser = new QueryParser("content", analyzer);
-		    String line;
+		    String phrase;
 
-		    while ( (line = in.readLine()) != null ) {
-		    	if ( line.trim().length() == 0)
+		    while ( (phrase = in.readLine()) != null ) {
+		    	if ( phrase.trim().length() == 0)
 		    		continue;
-		    	
-		    	Query query = parser.parse(line);
-		    	
-				System.out.println("-".repeat(80));
-		    	System.out.println("Searching for: " + query.toString());
-				System.out.println("-".repeat(80));
 
-		    	TopDocs searchResults = searcher.search(query, 100);
-		    	
+		    	System.out.print("Searching for: ");
+		    	System.out.print(phrase);
+		    	System.out.print(lnSeperator);
+
+		    	TopDocs searchResults = searchPhrase(phrase, field);
+
 				for (ScoreDoc sd : searchResults.scoreDocs)
 			    {
 			        Document d = searcher.doc(sd.doc);
-			        System.out.println("Document Name : " + d.get("title")
-			                    + "  :: Content : " + d.get("content")
-			                    + "  :: Score : " + sd.score);
+
+			        bufferedWriter.write(phrase);
+			        bufferedWriter.write("\t");
+			        bufferedWriter.write(d.get("title"));
+	        		bufferedWriter.write("\t" );
+	        		bufferedWriter.write(d.get("content"));
+					bufferedWriter.write(lnSeperator);
 			    }
 
-		    	
-		        System.out.println("Total Results :: " + searchResults.totalHits);
-		    	
+		        System.out.print("Total Results ");
+		        System.out.print(searchResults.totalHits);
+		        System.out.print(lnSeperator);
 		    }
 	    }
-	    reader.close();		
+	    bufferedWriter.close();
+	}
+
+	/*
+	 * Search for specific phrase or term in the loaded index
+	 * @param phrase Phrase or term to search for
+	 * @param field Name of the field to search
+	 * @return TopDocs results
+	 */
+	public TopDocs searchPhrase(String phrase, String field) throws IOException {
+		PhraseQuery query = new PhraseQuery(field, phrase.split(" "));
+    	return searcher.search(query, searchLimit);
+	}
+	
+	/* 
+	 * Release buffers
+	 */
+	public void close() {
+	    try {
+			reader.close();
+			dirIndex.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
