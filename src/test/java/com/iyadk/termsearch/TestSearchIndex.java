a903c683dee86aaf94a6076583a6c8a272be9663
Iyad Kandalaft <ik@iyadk.com>
2019-11-06 21:32:32 -0500

    Simplifying tests to facilitate readability

--- src/test/java/com/iyadk/termsearch/TestSearchIndex.java
+++ src/test/java/com/iyadk/termsearch/TestSearchIndex.java
@@ -19,10 +19,14 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestSearchIndex {
+
 	String indexDir;
 	String termsFile;
 	SearchIndex indexSearcher;
 
+	// Field to search for most tests
+	String field = "content";
+	
 	@BeforeEach
 	void setUp() throws Exception {
 		indexDir = Files.createTempDirectory("lucene-index-test").toString();
@@ -31,7 +35,7 @@ class TestSearchIndex {
 		termsFile = getClass().getClassLoader().getResource("test-terms.txt").getFile().toString();
 
 		/*
-		 * Create a known index
+		 * Create a known index from the test-courpus.txt resource
 		 */
 		IndexCreator indexCreator = new IndexCreator(corpusFile, indexDir);
 		indexCreator.create();
@@ -41,7 +45,6 @@ class TestSearchIndex {
 		try {
 			indexSearcher = new SearchIndex(termsFile, outputFile.toString(), indexDir);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -52,240 +55,184 @@ class TestSearchIndex {
 	}
 
 	@Test
-	void testSearchTermLCase() {
-		String field = "content";
+	void testSearchTermLCase() throws IOException {
 		String phrase = "cookie";
-		try {
-			TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			assertNotEquals(0, results.scoreDocs.length,
-					String.format("No results found for search phrase/term: %s", phrase));
-			assertEquals(1, results.scoreDocs.length,
-					String.format("Incorrect number of results were found for search phrase: %s", phrase));
+		TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			for (ScoreDoc sd : results.scoreDocs) {
-				Document d = indexSearcher.searcher.doc(sd.doc);
+		assertNotEquals(0, results.scoreDocs.length,
+				String.format("No results found for search phrase/term: %s", phrase));
+		assertEquals(1, results.scoreDocs.length,
+				String.format("Incorrect number of results were found for search phrase: %s", phrase));
 
-				// Lowecase should match
-				assertTrue(String.format("Lowecase result was not matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text that will match the term: cookie"));
-				// Camelcase should not match
-				assertFalse(String.format("CamelCase result matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text that will match the term: Cookie"));
-				// Uppercase should not match
-				assertFalse(String.format("Uppercase result matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text that will match the term: COOKIE"));
-			}
+		for (ScoreDoc sd : results.scoreDocs) {
+			Document d = indexSearcher.searcher.doc(sd.doc);
 
-		} catch (IOException e) {
-			fail("Exception raised while searching index");
-			e.printStackTrace();
-			return;
+			// Lowecase should match
+			assertTrue(String.format("Lowecase result was not matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text that will match the term: cookie"));
+			// Camelcase should not match
+			assertFalse(String.format("CamelCase result matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text that will match the term: Cookie"));
+			// Uppercase should not match
+			assertFalse(String.format("Uppercase result matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text that will match the term: COOKIE"));
 		}
 	}
 
 	@Test
-	void testSearchTermCCase() {
-		String field = "content";
+	void testSearchTermCCase() throws IOException {
 		String phrase = "Cookie";
-		try {
-			TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			assertNotEquals(0, results.scoreDocs.length,
-					String.format("No results found for search phrase/term: %s", phrase));
-			assertEquals(1, results.scoreDocs.length,
-					String.format("Incorrect number of results were found for search phrase: %s", phrase));
+		TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			for (ScoreDoc sd : results.scoreDocs) {
-				Document d = indexSearcher.searcher.doc(sd.doc);
+		assertNotEquals(0, results.scoreDocs.length,
+				String.format("No results found for search phrase/term: %s", phrase));
+		assertEquals(1, results.scoreDocs.length,
+				String.format("Incorrect number of results were found for search phrase: %s", phrase));
 
-				// Camelcase should match
-				assertTrue(String.format("CamelCase results was not matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text that will match the term: Cookie"));
-				// Lowecase should not match
-				assertFalse(String.format("Lowercase result matched found for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text that will match the term: cookie"));
-				// Uppercase should match
-				assertFalse(String.format("Uppercase result matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text that will match the term: COOKIE"));
-			}
+		for (ScoreDoc sd : results.scoreDocs) {
+			Document d = indexSearcher.searcher.doc(sd.doc);
 
-		} catch (IOException e) {
-			fail("Exception raised while searching index");
-			e.printStackTrace();
-			return;
+			// Camelcase should match
+			assertTrue(String.format("CamelCase results was not matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text that will match the term: Cookie"));
+			// Lowecase should not match
+			assertFalse(String.format("Lowercase result matched found for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text that will match the term: cookie"));
+			// Uppercase should match
+			assertFalse(String.format("Uppercase result matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text that will match the term: COOKIE"));
 		}
 	}
 
 	@Test
-	void testSearchTermUCase() {
-		String field = "content";
+	void testSearchTermUCase() throws IOException {
 		String phrase = "COOKIE";
-		try {
-			TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			assertNotEquals(0, results.scoreDocs.length,
-					String.format("No results found for search phrase/term: %s", phrase));
-			assertEquals(1, results.scoreDocs.length,
-					String.format("Incorrect number of results were found for search phrase: %s", phrase));
+		TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			for (ScoreDoc sd : results.scoreDocs) {
-				Document d = indexSearcher.searcher.doc(sd.doc);
+		assertNotEquals(0, results.scoreDocs.length,
+				String.format("No results found for search phrase/term: %s", phrase));
+		assertEquals(1, results.scoreDocs.length,
+				String.format("Incorrect number of results were found for search phrase: %s", phrase));
 
-				// Uppercase should match
-				assertTrue(String.format("Uppercase result was not matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text that will match the term: COOKIE"));
-				// Lowecase should not match
-				assertFalse(String.format("Lowercase result matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text that will match the term: cookie"));
-				// Camelcase should not match
-				assertFalse(String.format("CamelCase result matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text that will match the term: Cookie"));
-			}
+		for (ScoreDoc sd : results.scoreDocs) {
+			Document d = indexSearcher.searcher.doc(sd.doc);
 
-		} catch (IOException e) {
-			fail("Exception raised while searching index");
-			e.printStackTrace();
-			return;
+			// Uppercase should match
+			assertTrue(String.format("Uppercase result was not matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text that will match the term: COOKIE"));
+			// Lowecase should not match
+			assertFalse(String.format("Lowercase result matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text that will match the term: cookie"));
+			// Camelcase should not match
+			assertFalse(String.format("CamelCase result matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text that will match the term: Cookie"));
 		}
 	}
 
 	@Test
-	void testSearchPhraseLCase() {
-		String field = "content";
+	void testSearchPhraseLCase() throws IOException {
 		String phrase = "two words";
-		try {
-			TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			assertNotEquals(0, results.scoreDocs.length,
-					String.format("No results found for search phrase/term: %s", phrase));
-			assertEquals(1, results.scoreDocs.length,
-					String.format("Incorrect number of results were found for search phrase: %s", phrase));
+		TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			for (ScoreDoc sd : results.scoreDocs) {
-				Document d = indexSearcher.searcher.doc(sd.doc);
+		assertNotEquals(0, results.scoreDocs.length,
+				String.format("No results found for search phrase/term: %s", phrase));
+		assertEquals(1, results.scoreDocs.length,
+				String.format("Incorrect number of results were found for search phrase: %s", phrase));
 
-				assertTrue(String.format("Lower case result was not matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text where a term is composed of two words"));
-				assertFalse(String.format("Uppercase phrase was matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text where a term is composed of Two Words with capitals"));
-			}
+		for (ScoreDoc sd : results.scoreDocs) {
+			Document d = indexSearcher.searcher.doc(sd.doc);
 
-		} catch (IOException e) {
-			fail("Exception raised while searching index");
-			e.printStackTrace();
-			return;
+			assertTrue(String.format("Lower case result was not matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text where a term is composed of two words"));
+			assertFalse(String.format("Uppercase phrase was matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text where a term is composed of Two Words with capitals"));
 		}
 	}
 
 	@Test
-	void testSearchPhraseUCase() {
-		String field = "content";
+	void testSearchPhraseUCase() throws IOException {
 		String phrase = "Two Words";
-		try {
-			TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			assertNotEquals(0, results.scoreDocs.length,
-					String.format("No results found for search phrase/term: %s", phrase));
-			assertEquals(1, results.scoreDocs.length,
-					String.format("Incorrect number of results were found for search phrase: %s", phrase));
+		TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			for (ScoreDoc sd : results.scoreDocs) {
-				Document d = indexSearcher.searcher.doc(sd.doc);
+		assertNotEquals(0, results.scoreDocs.length,
+				String.format("No results found for search phrase/term: %s", phrase));
+		assertEquals(1, results.scoreDocs.length,
+				String.format("Incorrect number of results were found for search phrase: %s", phrase));
 
-				assertFalse(String.format("Lower case result was matched for phrase/term: %s", phrase),
-						d.get(field).contentEquals("This is sample text where a term is composed of two words"));
-				assertTrue(String.format("Uppercase phrase was matched for phrase/term: %s", phrase), d.get(field)
-						.contentEquals("This is sample text where a term is composed of Two Words with capitals"));
-			}
+		for (ScoreDoc sd : results.scoreDocs) {
+			Document d = indexSearcher.searcher.doc(sd.doc);
 
-		} catch (IOException e) {
-			fail("Exception raised while searching index");
-			e.printStackTrace();
-			return;
+			assertFalse(String.format("Lower case result was matched for phrase/term: %s", phrase),
+					d.get(field).contentEquals("This is sample text where a term is composed of two words"));
+			assertTrue(String.format("Uppercase phrase was matched for phrase/term: %s", phrase), d.get(field)
+					.contentEquals("This is sample text where a term is composed of Two Words with capitals"));
 		}
 	}
 
 	@Test
-	void testSearchInsideQuotes() {
-		String field = "content";
+	void testSearchInsideQuotes() throws IOException {
 		String phrase = "quote";
-		try {
-			TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			assertNotEquals(0, results.scoreDocs.length,
-					String.format("No results found for search phrase/term: %s", phrase));
+		TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			assertEquals(3, results.scoreDocs.length,
-					String.format("Incorrect number of results were found for search phrase: %s", phrase));
+		assertNotEquals(0, results.scoreDocs.length,
+				String.format("No results found for search phrase/term: %s", phrase));
 
-			for (ScoreDoc sd : results.scoreDocs) {
-				Document d = indexSearcher.searcher.doc(sd.doc);
+		assertEquals(3, results.scoreDocs.length,
+				String.format("Incorrect number of results were found for search phrase: %s", phrase));
 
-				assertTrue(String.format("Terms found inside quotes for phrase/term: %s", phrase),
-						(d.get(field).contentEquals("This is sample text where a term is surrounded by \"quote\"")
-								|| d.get(field).contentEquals("This is sample text where a term ends with \"a quote\"")
-								|| d.get(field).contentEquals(
-										"This is sample text where a term starts with a \"quote only\"")));
-			}
+		for (ScoreDoc sd : results.scoreDocs) {
+			Document d = indexSearcher.searcher.doc(sd.doc);
 
-		} catch (IOException e) {
-			fail("Exception raised while searching index");
-			e.printStackTrace();
-			return;
+			assertTrue(String.format("Terms found inside quotes for phrase/term: %s", phrase),
+					(d.get(field).contentEquals("This is sample text where a term is surrounded by \"quote\"")
+							|| d.get(field).contentEquals("This is sample text where a term ends with \"a quote\"")
+							|| d.get(field).contentEquals(
+									"This is sample text where a term starts with a \"quote only\"")));
 		}
 	}
 
 	@Test
-	void testSearchPhraseHyphen() {
-		String field = "content";
+	void testSearchPhraseHyphen() throws IOException {
 		String phrase = "hyphen";
-		try {
-			TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			assertNotEquals(0, results.scoreDocs.length,
-					String.format("No results found for search phrase/term: %s", phrase));
-			assertTrue(String.format("More than two results were found for search phrase: %s", phrase),
-					(results.scoreDocs.length < 3));
+		TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			for (ScoreDoc sd : results.scoreDocs) {
-				Document d = indexSearcher.searcher.doc(sd.doc);
+		assertNotEquals(0, results.scoreDocs.length,
+				String.format("No results found for search phrase/term: %s", phrase));
+		assertTrue(String.format("More than two results were found for search phrase: %s", phrase),
+				(results.scoreDocs.length < 3));
 
-				assertTrue(String.format("Result without a hyphen was matched for phrase/term: %s", phrase),
-						d.get(field).equals("This is sample text where a term is suffixed by a hyphen-term")
-								|| d.get(field).equals("This is sample text where a term is prefixed by a term-hyphen"));
-			}
+		for (ScoreDoc sd : results.scoreDocs) {
+			Document d = indexSearcher.searcher.doc(sd.doc);
 
-		} catch (IOException e) {
-			fail("Exception raised while searching index");
-			e.printStackTrace();
-			return;
+			assertTrue(String.format("Result without a hyphen was matched for phrase/term: %s", phrase),
+					d.get(field).equals("This is sample text where a term is suffixed by a hyphen-term")
+							|| d.get(field).equals("This is sample text where a term is prefixed by a term-hyphen"));
 		}
 	}
 	
 	@Test
-	void testSearchPhraseIgnoreFirstWord() {
-		String field = "content";
+	void testSearchPhraseIgnoreFirstWord() throws IOException {
 		String phrase = "My";
-		
-		try {
-			TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			assertNotEquals(0, results.scoreDocs.length,
-					String.format("No results found for search phrase/term: %s", phrase));
-			assertEquals(1, results.scoreDocs.length,
-					String.format("Incorrect number of results were found for search phrase: %s", phrase));
+		TopDocs results = indexSearcher.searchPhrase(phrase, field);
 
-			for (ScoreDoc sd : results.scoreDocs) {
-				Document d = indexSearcher.searcher.doc(sd.doc);
+		assertNotEquals(0, results.scoreDocs.length,
+				String.format("No results found for search phrase/term: %s", phrase));
+		assertEquals(1, results.scoreDocs.length,
+				String.format("Incorrect number of results were found for search phrase: %s", phrase));
 
-				assertTrue(String.format("Result without a hyphen was matched for phrase/term: %s", phrase),
-						d.get(field).equals("My sample text where a the first term My is not matched but the second one is"));
-			}
+		for (ScoreDoc sd : results.scoreDocs) {
+			Document d = indexSearcher.searcher.doc(sd.doc);
 
-		} catch (IOException e) {
-			fail("Exception raised while searching index");
-			e.printStackTrace();
+			assertTrue(String.format("Result without a hyphen was matched for phrase/term: %s", phrase),
+					d.get(field).equals("My sample text where a the first term My is not matched but the second one is"));
 		}
 	}
 
