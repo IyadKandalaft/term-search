7e340a996ff27153466de82ac05b6a1eec2bb704
Iyad Kandalaft <ik@iyadk.com>
2019-12-30 11:34:57 -0500

    Added common abbreviation exclusions to sentence first word detection

--- src/main/java/com/iyadk/termsearch/UniqueAnalyzer.java
+++ src/main/java/com/iyadk/termsearch/UniqueAnalyzer.java
@@ -1,8 +1,9 @@
 package com.iyadk.termsearch;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Map;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.custom.CustomAnalyzer;
@@ -15,11 +16,28 @@ public class UniqueAnalyzer{
 	
 	public Analyzer analyzer;
 	
+
 	private UniqueAnalyzer() throws IOException {
-		Map<String, String> firstWordRegExpFilter = new HashMap<String,String>();
+		// Abbreviations - exclude trailing period
+		ArrayList<String> abbreviations = new ArrayList<>(Arrays.asList(
+				"Dr", "Prof", "Gen", "Rep", "Sen", "St", "Hon",
+				"Mr", "Mrs", "Ms", "Mme", "Messrs", 
+				"Sr", "Jr",
+				"Ph.D", "M.D", "B.A", "M.A", "D.D.S",
+				"U.S", "U.S.A", "e.g", "i.e", "etc", "et al", "ca", "O.K",
+				"A.M", "P.M","A.D", "B.C"));
+		
+		// Escape all "." with "\\" to prepare it for the regex 
+		abbreviations.replaceAll(s -> s.replace(".", "\\."));
+		// Pattern matching sentence-ending punctuation but ignores periods after common abbrev: "Mr. Author"
+		String regexPattern = "(\"|!|\\?|(?<!" + String.join("|", abbreviations) + ")\\.|;|:)(\\s*)[A-Z](\\w+)[^\"']";
+
+		HashMap<String, String> firstWordRegExpFilter = new HashMap<String,String>();
 		// RegExp to remove the first word of every sentence
-		firstWordRegExpFilter.put("pattern", "([\"!.;:]\\s*)[A-Z]\\w+[^\"']");
-		firstWordRegExpFilter.put("replacement", "$1");
+		//firstWordRegExpFilter.put("pattern", "([\"!.;:])(\\s*)[A-Z](\\w+)[^\"']");
+		//firstWordRegExpFilter.put("replacement", "$1$2*$3");
+		firstWordRegExpFilter.put("pattern", regexPattern);
+		firstWordRegExpFilter.put("replacement", "$1$2*$3");
 
 		analyzer = CustomAnalyzer.builder()
 		.addCharFilter(PatternReplaceCharFilterFactory.class, firstWordRegExpFilter)
